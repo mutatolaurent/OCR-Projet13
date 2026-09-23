8d59c427ccd5ddb763122083b99c2fc4189ae103
mutatolaurent <mutato.laurent@gmail.com>
2026-09-23 11:08:58 +0200

    Ajout formulaire de connexion + Gestion du menu en fonction du user connecté + connexion automatique suite à une inscription réussie

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -14,7 +14,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
-#[UniqueEntity(fields: ['email'], message: '⚠️ Il existe déjà un compte avec cet email')]
+#[UniqueEntity(fields: ['email'], message: 'Il existe déjà un compte avec cet email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
